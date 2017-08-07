750eea210bda3204eddd3ff4ed6e29a147695913
Mike Gahagan <mgahagan@redhat.com>
2017-08-07 11:57:28 -0400

    Add changes From Merlin M  - add hosts, tags, and roles fields to the playbook (yields a usable playbook for running tests)

--- gen_playbook.py
+++ gen_playbook.py
@@ -3,13 +3,18 @@
 import os
 import yaml
 
-playbook={}
+playbook = [{'hosts': 'localhost',
+             'tags': ['classic', 'container', 'atomic'],
+             'roles': [{'role': 'standard-test-beakerlib',
+                        'tests': [],
+                        'required_packages': []}]
+           }]
 tests=[]
 wd=os.getcwd()
 for directory, dirnames, filenames in os.walk(wd):
     if 'runtest.sh' in filenames:
         tests.append(os.path.relpath(directory))
-playbook['tests'] = tests
+playbook[0]['roles'][0]['tests'] = tests
 
 #print playbook
 print yaml.dump(playbook,default_flow_style=False)
